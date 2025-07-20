33c3ae2bbb331da1fd3e01aaf54849e332504b30
5w7tch <nurch22@freeuni.edu.ge>
2025-07-20 19:32:08 +0400

    changes

--- src/App.js
+++ src/App.js
@@ -66,7 +66,8 @@ const App = () => {
               left: `${Math.random() * 100}%`,
               top: `${Math.random() * 100}%`,
               animationDelay: `${Math.random() * 3}s`,
-              animationDuration: `${3 + Math.random() * 4}s`
+              animationDuration: `${3 + Math.random() * 4}s`,
+              zIndex: 9999
             }}
           />
         ))}
